b946cf8b0a570245ee29b5f1059837fd00fab46d
Ivan Vermeyen <ivan@vermeyen.org>
2023-03-31 18:27:01 +0200

    Add code

--- src/UrlBuilder.php
+++ src/UrlBuilder.php
@@ -4,5 +4,268 @@ namespace CodeZero\UrlBuilder;
 
 class UrlBuilder
 {
-    //
+    /**
+     * The parsed URL parts.
+     *
+     * @var array
+     */
+    protected $urlParts;
+
+    /**
+     * Crate a new UrlBuilder instance.
+     *
+     * @param string $url
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public static function make(string $url): UrlBuilder
+    {
+        return new self($url);
+    }
+
+    /**
+     * Crate a new UrlBuilder instance.
+     *
+     * @param string $url
+     */
+    public function __construct(string $url)
+    {
+        $this->urlParts = parse_url($url) ?: [];
+    }
+
+    /**
+     * Create a string from URL parts.
+     *
+     * @param bool $absolute
+     *
+     * @return string
+     */
+    public function build(bool $absolute = true): string
+    {
+        $url = '';
+        $port = $this->getPort();
+        $query =  $this->getQueryString();
+
+        if ($absolute === true) {
+            $port = $port ? ":{$port}" : '';
+            $url .= $this->getScheme() . '://' . $this->getHost() . $port;
+        }
+
+        if ($absolute === false || $this->getPath() !== '/' || $query !== '') {
+            $url .= $this->getPath();
+        }
+
+        $url .= $query ? '?' . $query : '';
+
+        return $url;
+    }
+
+    /**
+     * Get the scheme.
+     *
+     * @return string
+     */
+    public function getScheme(): string
+    {
+        return $this->get('scheme');
+    }
+
+    /**
+     * Set the scheme.
+     *
+     * @param string $scheme
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public function setScheme(string $scheme): UrlBuilder
+    {
+        $this->set('scheme', $scheme);
+
+        return $this;
+    }
+
+    /**
+     * Get the host.
+     *
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->get('host');
+    }
+
+    /**
+     * Set the host.
+     *
+     * @param string $host
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public function setHost(string $host): UrlBuilder
+    {
+        $this->set('host', $host);
+
+        return $this;
+    }
+
+    /**
+     * Get the port.
+     *
+     * @return string
+     */
+    public function getPort(): string
+    {
+        return $this->get('port');
+    }
+
+    /**
+     * Set the port.
+     *
+     * @param string|int|null $port
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public function setPort($port): UrlBuilder
+    {
+        $this->set('port', $port);
+
+        return $this;
+    }
+
+    /**
+     * Get the path.
+     *
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->get('path');
+    }
+
+    /**
+     * Set the path.
+     *
+     * @param string $path
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public function setPath(string $path): UrlBuilder
+    {
+        $this->set('path', '/' . trim($path, '/'));
+
+        return $this;
+    }
+
+    /**
+     * Get the slugs.
+     *
+     * @return array
+     */
+    public function getSlugs(): array
+    {
+        $path = $this->getPath();
+
+        if ($path === '/') {
+            return [];
+        }
+
+        return explode('/', trim($path, '/'));
+    }
+
+    /**
+     * Set the slugs.
+     *
+     * @param array $slugs
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public function setSlugs(array $slugs): UrlBuilder
+    {
+        $this->setPath('/' . join('/', $slugs));
+
+        return $this;
+    }
+
+    /**
+     * Get the query string.
+     *
+     * @return string
+     */
+    public function getQueryString(): string
+    {
+        return $this->get('query');
+    }
+
+    /**
+     * Set the query string.
+     *
+     * @param string $query
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public function setQueryString(string $query): UrlBuilder
+    {
+        $this->set('query', ltrim($query, '?'));
+
+        return $this;
+    }
+
+    /**
+     * Get the query string as an array.
+     *
+     * @return array
+     */
+    public function getQuery(): array
+    {
+        $query = $this->get('query');
+
+        if ($query === '') {
+            return [];
+        }
+
+        parse_str($query, $queryArray);
+
+        return $queryArray;
+    }
+
+    /**
+     * Set the query string parameters.
+     *
+     * @param array $query
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    public function setQuery(array $query): UrlBuilder
+    {
+        $this->set('query', http_build_query($query));
+
+        return $this;
+    }
+
+    /**
+     * Get the value of a URL part.
+     *
+     * @param string $part
+     *
+     * @return string
+     */
+    protected function get(string $part): string
+    {
+        return $this->urlParts[$part] ?? '';
+    }
+
+    /**
+     * Set a URL part to a new value.
+     *
+     * @param string $part
+     * @param string|null $value
+     *
+     * @return \CodeZero\UrlBuilder\UrlBuilder
+     */
+    protected function set(string $part, ?string $value): UrlBuilder
+    {
+        $this->urlParts[$part] = strval($value);
+
+        return $this;
+    }
 }
--- tests/Unit/UrlBuilderTest.php
+++ tests/Unit/UrlBuilderTest.php
@@ -3,8 +3,192 @@
 namespace CodeZero\UrlBuilder\Tests\Unit;
 
 use CodeZero\UrlBuilder\Tests\TestCase;
+use CodeZero\UrlBuilder\UrlBuilder;
 
 class UrlBuilderTest extends TestCase
 {
-    //
+    /** @test */
+    public function it_builds_a_url()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $this->assertEquals('http://www.example.com:8000/abc/def?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/abc/def?ref=testcase&foo=bar', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_gets_url_parts()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $this->assertEquals('http', $urlBuilder->getScheme());
+        $this->assertEquals('www.example.com', $urlBuilder->getHost());
+        $this->assertEquals('8000', $urlBuilder->getPort());
+        $this->assertEquals('/abc/def', $urlBuilder->getPath());
+        $this->assertEquals(['abc', 'def'], $urlBuilder->getSlugs());
+        $this->assertEquals('ref=testcase&foo=bar', $urlBuilder->getQueryString());
+        $this->assertEquals(['ref' => 'testcase', 'foo' => 'bar'], $urlBuilder->getQuery());
+    }
+
+    /** @test */
+    public function it_sets_the_scheme()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setScheme('https');
+
+        $this->assertEquals('https', $urlBuilder->getScheme());
+        $this->assertEquals('https://www.example.com:8000/abc/def?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/abc/def?ref=testcase&foo=bar', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_sets_the_host()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setHost('example.test');
+
+        $this->assertEquals('example.test', $urlBuilder->getHost());
+        $this->assertEquals('http://example.test:8000/abc/def?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/abc/def?ref=testcase&foo=bar', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_sets_the_port()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setPort(8080);
+
+        $this->assertEquals('8080', $urlBuilder->getPort());
+        $this->assertEquals('http://www.example.com:8080/abc/def?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/abc/def?ref=testcase&foo=bar', $urlBuilder->build(false));
+
+        $urlBuilder->setPort(null);
+
+        $this->assertEquals('', $urlBuilder->getPort());
+        $this->assertEquals('http://www.example.com/abc/def?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/abc/def?ref=testcase&foo=bar', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_sets_the_path()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setPath('lorem/ipsum');
+
+        $this->assertEquals('/lorem/ipsum', $urlBuilder->getPath());
+        $this->assertEquals(['lorem', 'ipsum'], $urlBuilder->getSlugs());
+        $this->assertEquals('http://www.example.com:8000/lorem/ipsum?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/lorem/ipsum?ref=testcase&foo=bar', $urlBuilder->build(false));
+
+        $urlBuilder->setPath('/lorem/ipsum');
+
+        $this->assertEquals('/lorem/ipsum', $urlBuilder->getPath());
+        $this->assertEquals(['lorem', 'ipsum'], $urlBuilder->getSlugs());
+        $this->assertEquals('http://www.example.com:8000/lorem/ipsum?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/lorem/ipsum?ref=testcase&foo=bar', $urlBuilder->build(false));
+
+        $urlBuilder->setPath('/');
+
+        $this->assertEquals('/', $urlBuilder->getPath());
+        $this->assertEquals([], $urlBuilder->getSlugs());
+        $this->assertEquals('http://www.example.com:8000/?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/?ref=testcase&foo=bar', $urlBuilder->build(false));
+
+        $urlBuilder->setPath('');
+
+        $this->assertEquals('/', $urlBuilder->getPath());
+        $this->assertEquals([], $urlBuilder->getSlugs());
+        $this->assertEquals('http://www.example.com:8000/?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/?ref=testcase&foo=bar', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_sets_the_slugs()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setSlugs(['lorem', 'ipsum']);
+
+        $this->assertEquals('/lorem/ipsum', $urlBuilder->getPath());
+        $this->assertEquals(['lorem', 'ipsum'], $urlBuilder->getSlugs());
+        $this->assertEquals('http://www.example.com:8000/lorem/ipsum?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/lorem/ipsum?ref=testcase&foo=bar', $urlBuilder->build(false));
+
+        $urlBuilder->setSlugs([]);
+
+        $this->assertEquals('/', $urlBuilder->getPath());
+        $this->assertEquals([], $urlBuilder->getSlugs());
+        $this->assertEquals('http://www.example.com:8000/?ref=testcase&foo=bar', $urlBuilder->build());
+        $this->assertEquals('/?ref=testcase&foo=bar', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_sets_the_query_string()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setQueryString('a=b&c=d');
+
+        $this->assertEquals('a=b&c=d', $urlBuilder->getQueryString());
+        $this->assertEquals(['a' => 'b', 'c' => 'd'], $urlBuilder->getQuery());
+        $this->assertEquals('http://www.example.com:8000/abc/def?a=b&c=d', $urlBuilder->build());
+        $this->assertEquals('/abc/def?a=b&c=d', $urlBuilder->build(false));
+
+        $urlBuilder->setQueryString('');
+
+        $this->assertEquals('', $urlBuilder->getQueryString());
+        $this->assertEquals([], $urlBuilder->getQuery());
+        $this->assertEquals('http://www.example.com:8000/abc/def', $urlBuilder->build());
+        $this->assertEquals('/abc/def', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_sets_the_query_parameters()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setQuery(['a' => 'b', 'c' => 'd']);
+
+        $this->assertEquals('a=b&c=d', $urlBuilder->getQueryString());
+        $this->assertEquals(['a' => 'b', 'c' => 'd'], $urlBuilder->getQuery());
+        $this->assertEquals('http://www.example.com:8000/abc/def?a=b&c=d', $urlBuilder->build());
+        $this->assertEquals('/abc/def?a=b&c=d', $urlBuilder->build(false));
+
+        $urlBuilder->setQuery([]);
+
+        $this->assertEquals('', $urlBuilder->getQueryString());
+        $this->assertEquals([], $urlBuilder->getQuery());
+        $this->assertEquals('http://www.example.com:8000/abc/def', $urlBuilder->build());
+        $this->assertEquals('/abc/def', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_adds_a_slash_between_the_host_and_query_string()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/abc/def?ref=testcase&foo=bar');
+
+        $urlBuilder->setQueryString('a=b&c=d');
+        $urlBuilder->setPath('');
+
+        $this->assertEquals('http://www.example.com:8000/?a=b&c=d', $urlBuilder->build());
+        $this->assertEquals('/?a=b&c=d', $urlBuilder->build(false));
+    }
+
+    /** @test */
+    public function it_removes_a_trailing_slash_after_the_host()
+    {
+        $urlBuilder = new UrlBuilder('http://www.example.com:8000/');
+
+        $this->assertEquals('http://www.example.com:8000', $urlBuilder->build());
+        $this->assertEquals('/', $urlBuilder->build(false));
+
+        $urlBuilder = new UrlBuilder('http://www.example.com/');
+
+        $this->assertEquals('http://www.example.com', $urlBuilder->build());
+        $this->assertEquals('/', $urlBuilder->build(false));
+    }
 }
